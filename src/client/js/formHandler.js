63b5931c1cacefcefced2e322d52f1aa8877fb87
Viviane Costa <vivianecostapin@gmail.com>
2021-03-18 08:33:46 -0300

    Adding api call to OpenWeatherAPI

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,22 @@
+const apiToken = '';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+
+
+async function getWeatherInfo(zipCode) {
+    const url = `${baseUrl}?zip=${zipCode}&appId=${apiToken}&units=imperial`;
+    const response = await fetch(url);
+    const { status } = response;
+    const newData = await response.json();
+    if (status !== 200) {
+      if (newData.message === 'city not found') {
+        return Promise.reject('cityNotFound');
+      }
+      return Promise.reject('statusCodeError');
+    }
+    return newData;
+  }
+  
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -6,10 +25,9 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
+    getWeatherInfo(formText)
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = `${res.main.temp} °F in ${res.name} `;
     })
 }
 
